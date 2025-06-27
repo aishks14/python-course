c1be6674268778221837a6e68f67b61233b79499
aishks14 <aishks14@outlook.com>
2025-06-27 10:45:49 +0530

    chapter 6 mistake corrections and normal space added after merge issue

--- chapter6-PS/problem4.py
+++ chapter6-PS/problem4.py
@@ -1,4 +1,5 @@
 # WAP in python to check whether a given username contains less than 10 characters (error message to be printed), starts with an alphabet, and contains only alphanumeric characters.
+
 username = input("Enter a username: ")
 if len(username) > 10 and username[0].isalpha() and username.isalnum():
     print("The username is valid.")
