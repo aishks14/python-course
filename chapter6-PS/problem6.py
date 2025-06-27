e0167558653ee4f1df76d4aced079d1d4705e21c
aishks14 <aishks14@outlook.com>
2025-06-27 10:41:22 +0530

    chapter 6 practise set normal commit

--- chapter6-PS/problem6.py
+++ chapter6-PS/problem6.py
@@ -1,4 +1,5 @@
 # WAP in python to check whether a post is talking about python or not.
+
 post = input("Enter a post: ")
 if "python" in post.lower():
     print("The post is talking about Python.")
